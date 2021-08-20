c1c72999c89ac60e88878fe57aeade088a4d4222
Jan <deveraj.slash@gmail.com>
2021-08-20 23:20:48 +0800

    first commit to this project

---
+++ index.js
@@ -0,0 +1,130 @@
+const express = require ('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const randomString = require('randomstring');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+// template of users' object:
+// userId: socket.id
+// username: depends on the user
+// roomNumber: depends if creating or joining
+const users = [];
+
+app.use(express.static("public"));
+
+// checks for client connecting to server
+io.on('connection', (socket) => {
+    users.push({ id: socket.id });
+    console.log('user connected', socket.id, users);
+
+    // login events
+    socket.on('create-room-attempt', (username) => {
+        const userIndex = users.findIndex(user => user.id === socket.id);
+        let roomCode = randomString.generate({
+            length: 4,
+            charset: 'numeric'
+        });
+
+        // check if roomCode already exists
+        while (users.findIndex(user => user.roomCode === roomCode) !== -1) {
+            roomCode = randomString.generate({
+                length: 4,
+                charset: 'numeric'
+            });
+        }
+
+        // update user array with the needed data
+        if (userIndex !== -1) {
+            users[userIndex].username = username;
+            users[userIndex].roomCode = roomCode;
+            socket.join(roomCode);
+            socket.emit('create-room-successful', username, roomCode);
+            console.log(username, 'has entered room', roomCode, users);
+            io.to(roomCode).emit('create-room-notification', username, roomCode, users.filter(user => user.roomCode === roomCode));
+        } else {
+            socket.emit('create-room-failed');
+        }
+
+        // console.log(io.sockets.adapter.rooms);
+    });
+
+    socket.on('join-room-attempt', (username, roomCode) => {
+        const userIndex = users.findIndex(user => user.id === socket.id);
+
+        // check if (1) room does not exist, (2) username is already taken in room
+        // (3) successful login, (4) just a general error
+        if (users.findIndex(user => user.roomCode === roomCode) === -1) {
+            socket.emit('join-room-failed', 'room does not exist');
+        } else if (users.findIndex(user => user.username === username && user.roomCode === roomCode) !== -1) {
+            socket.emit('join-room-failed', 'username already exists in room');
+        } else if (userIndex !== -1) {
+            users[userIndex].username = username;
+            users[userIndex].roomCode = roomCode;
+            socket.join(roomCode);
+            socket.emit('join-room-successful', username, roomCode);
+            console.log(username, 'has entered room', roomCode, users);
+            io.to(roomCode).emit('join-room-notification', username, roomCode, users.filter(user => user.roomCode === roomCode));
+        } else {
+            socket.emit('join-room-failed', 'join room failed');
+        }
+
+        // console.log(io.sockets.adapter.rooms);
+    });
+
+    // chat room events
+    socket.on('leave-room-attempt', () => {
+        const userIndex = users.findIndex(user => user.id === socket.id);
+
+        // update user data (remove roomCode)
+        if (userIndex !== -1) {
+            const username = users[userIndex].username;
+            const roomCode = users[userIndex].roomCode;
+
+            delete users[userIndex].username;
+            delete users[userIndex].roomCode;
+
+            socket.leave(roomCode);
+            socket.emit('leave-room-successful', username, roomCode);
+            console.log(username, 'has left room', roomCode, users);
+            io.to(roomCode).emit('leave-room-notification', username, roomCode, users.filter(user => user.roomCode === roomCode));
+        } else {
+            socket.emit('leave-room-failed');
+        }
+
+        // console.log(io.sockets.adapter.rooms);
+    });
+
+    socket.on('message-to-server', (message) => {
+        const userIndex = users.findIndex(user => user.id === socket.id);
+        const roomCode = users[userIndex].roomCode;
+        const username = users[userIndex].username;
+
+        // broadcast message to everyone in room
+        io.to(roomCode).emit('message-from-server', username, message, socket.id);
+    })
+
+    // disconnected from server
+    socket.on('disconnect', () => {
+        const userIndex = users.findIndex(user => user.id === socket.id);
+        const username = users[userIndex].username;
+        const roomCode = users[userIndex].roomCode;
+
+        // remove user from users array
+        users.splice(userIndex, 1);
+
+        // notify everyone in room that user has left
+        if (username && roomCode) {
+            io.to(roomCode).emit('leave-room-notification', username, roomCode, users.filter(user => user.roomCode === roomCode));   
+        }
+
+        // console.log(io.sockets.adapter.rooms);
+    });
+});
+
+server.listen(port, () => {
+    console.log('listening on port ' + port);
+});
---
+++ public/script.js
@@ -0,0 +1,246 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // initializing variables
+    const socket = io();
+
+    // show default screen (login)
+    displayLoginScreen();
+
+    // socket login events needed
+    socket.on('create-room-successful', (username, roomCode) => {
+        displayChatScreen(username, roomCode);
+    })
+
+    socket.on('create-room-failed', () => {
+        alert('room creation failed');
+    });
+
+    socket.on('join-room-successful', (username, roomCode) => {
+        displayChatScreen(username, roomCode);
+    });
+
+    socket.on('join-room-failed', (message) => {
+        alert(message);
+    });
+
+    // socket chat events needed
+    socket.on('leave-room-successful', (username, roomCode) => {
+        displayLoginScreen(username, roomCode);
+    });
+
+    socket.on('leave-room-failed', () => {
+        alert('leave room failed');
+    });
+
+    socket.on('message-from-server', (username, message, id) => {
+        const messageContainer = document.createElement('div');
+        const latestMessage = document.querySelector('#message-list li:last-child');
+        const usernameOfLatestMessage = latestMessage.classList.contains('notification') ? '' : latestMessage.querySelector('.username-container').textContent;
+
+        if (usernameOfLatestMessage && usernameOfLatestMessage === username) {
+            messageContainer.textContent = message;
+            messageContainer.classList.add('message-container');
+            latestMessage.appendChild(messageContainer);
+        } else {
+            const messageItem = document.createElement('li');
+            const isMyMessage = socket.id === id;
+            const usernameContainer = document.createElement('div');
+
+            usernameContainer.textContent = username;
+            usernameContainer.classList.add('username-container');
+            messageContainer.textContent = message;
+            messageContainer.classList.add('message-container');
+            messageItem.classList.add(isMyMessage ? 'my-message' : 'their-message');
+            messageItem.appendChild(usernameContainer);
+            messageItem.appendChild(messageContainer);
+            document.getElementById('message-list').appendChild(messageItem);
+        }
+
+        messageContainer.scrollIntoView()
+    });
+
+    socket.on('create-room-notification', (username, roomCode, userList) => {
+        handleNotification(username, roomCode, userList, 'created');
+    });
+    socket.on('join-room-notification', (username, roomCode, userList) => {
+        handleNotification(username, roomCode, userList, 'joined');
+    });
+    socket.on('leave-room-notification', (username, roomCode, userList) => {
+        handleNotification(username, roomCode, userList, 'left');
+    });
+
+    function handleNotification (username, roomCode, userList, action) {
+        const messageItem = document.createElement('li');
+        const userItem = document.createElement('li');
+        const newUserList = userList.filter(user => user.id !== socket.id);
+
+        messageItem.textContent = `${username} has ${action} room ${roomCode}`;
+        messageItem.classList.add('notification');
+        userItem.textContent = 'Me';
+        document.getElementById('message-list').appendChild(messageItem);
+        document.getElementById('room-code').textContent = `Room ${roomCode}`;
+        document.getElementById('users-online').innerHTML = '';
+        document.getElementById('users-online').appendChild(userItem);
+
+        newUserList.forEach(user => {
+            const userItem = document.createElement('li');
+
+            userItem.textContent = user.username;
+            document.getElementById('users-online').appendChild(userItem);
+        })
+
+        window.dispatchEvent(new Event('resize'));
+        messageItem.scrollIntoView();
+    }
+
+    // display functions
+    function displayLoginScreen (username = "", roomCode = "") {
+        document.getElementById('output').innerHTML = `
+            <div id="login">
+                <h1>Chat demo</h1>
+                <form id="login-form" autocomplete="off">
+                    <input type="text" id="username" placeholder="Username" maxlength="12"/>
+                    <input type="text" id="room-code" class="is-valid" placeholder="Room code (leave empty to create room)" maxlength="4"/>
+                    <input type="submit" id="enter" value="Create" />
+                </form>
+            </div>
+        `;
+
+        const usernameField = document.getElementById('username');
+        const roomCodeField = document.getElementById('room-code');
+        const enterButton = document.getElementById('enter');
+        const loginForm = document.getElementById('login-form');
+
+        usernameField.oninput = () => {
+            usernameField.classList.remove('is-valid');
+            usernameField.classList.remove('is-invalid');
+
+            if (usernameField.value.match(/[0-9a-zA-Z]{4,12}/) && usernameField.value.match(/[0-9a-zA-Z]{4,12}/)[0] === usernameField.value) {
+                usernameField.classList.add('is-valid');
+            } else {
+                usernameField.classList.add('is-invalid');
+            }
+        }
+
+        roomCodeField.oninput = () => {
+            roomCodeField.classList.remove('is-valid');
+            roomCodeField.classList.remove('is-invalid');
+
+            if (roomCodeField.value.match(/[0-9]{4}/) && roomCodeField.value.match(/[0-9]{4}/)[0] === roomCodeField.value) {
+                roomCodeField.classList.add('is-valid');
+            } else if (!roomCodeField.value) {
+                roomCodeField.classList.add('is-valid');
+            } else {
+                roomCodeField.classList.add('is-invalid');
+            }
+
+            if (roomCodeField.value) {
+                enterButton.value = "Join";
+            } else {
+                enterButton.value = "Create";
+            }
+        }
+
+        loginForm.onsubmit = (e) => {
+            e.preventDefault();
+
+            if (usernameField.classList.contains('is-valid') && !roomCodeField.value) {
+                socket.emit('create-room-attempt', usernameField.value);
+            } else if (usernameField.classList.contains('is-valid') && roomCodeField.classList.contains('is-valid')) {
+                socket.emit('join-room-attempt', usernameField.value, roomCodeField.value);
+            }
+        }
+
+        if (username && roomCode) {
+            const inputEvent = new Event('input');
+
+            usernameField.value = username;
+            usernameField.dispatchEvent(inputEvent);
+            roomCodeField.value = roomCode;
+            roomCodeField.dispatchEvent(inputEvent);
+        }
+    }
+
+    function displayChatScreen () {
+        document.getElementById('output').innerHTML = `
+            <div id="chat">
+                <div id="chat-sidebar">
+                    <button id="leave-room">Leave room</button>
+                    <button id="hide-sidebar">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </button>
+                    <h2>People in this room:</h2>
+                    <ul id="users-online"></ul>
+                </div>
+                <div class="content">
+                    <div class="content-menu">
+                        <h2 id="room-code"></h2>
+                        <button id="show-sidebar">
+                            <i class="fa fa-bars" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                    <ul id="message-list"></ul>
+                    <form id="message-form" autocomplete="off">
+                        <input type="text" id="message-content" placeholder="Message"/>
+                        <input type="submit" id="send-message" value="Send" />
+                    </form>
+                </div>
+            </div>
+        `;
+
+        const leaveRoomButton = document.getElementById('leave-room');
+        const messageContentField = document.getElementById('message-content');
+        const messageForm = document.getElementById('message-form');
+        const showSidebarButton = document.getElementById('show-sidebar');
+        const hideSidebarButton = document.getElementById('hide-sidebar');
+        const chatSidebar = document.getElementById('chat-sidebar');
+
+        leaveRoomButton.onclick = () => {
+            socket.emit('leave-room-attempt');
+        }
+
+        messageForm.onsubmit = (e) => {
+            e.preventDefault();
+
+            if (messageContentField.value) {
+                socket.emit('message-to-server', messageContentField.value);
+                messageContentField.value = '';
+            }
+        }
+
+        showSidebarButton.onclick = () => {
+            chatSidebar.classList.add('active');
+        }
+
+        hideSidebarButton.onclick = () => {
+            chatSidebar.classList.remove('active');
+        }
+
+        function setSizes() {
+            const headerHeight = document.querySelector('#chat .content-menu').offsetHeight;
+            const messageFormHeight = document.getElementById('message-form').offsetHeight;
+            const windowHeight = window.innerHeight < window.outerHeight ? window.innerHeight : window.outerHeight;
+            const windowWidth = window.innerWidth < window.outerWidth ? window.innerWidth : window.outerWidth;
+            const messageListHeight = windowHeight - headerHeight - messageFormHeight;
+            const sidebarWidth = document.getElementById('chat-sidebar').offsetWidth;
+
+            document.getElementById('message-list').style.marginBottom = `${messageFormHeight}px`;
+            document.getElementById('message-list').style.height = `${messageListHeight}px`;
+
+            if (sidebarWidth >= 300 && windowWidth >= 768) {
+                document.querySelector('#chat .content').style.width = `${windowWidth - sidebarWidth}px`;
+            } else {
+                document.querySelector('#chat .content').style.width = '100%';
+            }
+
+            document.querySelectorAll('#chat > div').forEach(item => {
+                item.style.height = `${windowHeight}px`;
+            });
+
+            document.getElementById('message-content').style.width = `${document.getElementById('message-form').offsetWidth - document.getElementById('send-message').offsetWidth - 24}px`;
+        }
+
+        setSizes();
+        // window.onresize = setSizes;
+        window.addEventListener('resize', setSizes);
+    }
+})
